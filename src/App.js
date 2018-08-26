f7cb3d111d36fd0d4a5a8e0a0e866c2f601d65c0
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-25 20:08:09 -0400

    Added Categories to Store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
+
+import Categories from './Components/Categories.js'
+
 import FighterBackground from './Data/Images/FighterBackground.jpg'
-import SE5 from './Data/Images/SE5.jpeg'
-import BF109 from './Data/Images/BF109.jpeg'
-import F4 from './Data/Images/F4.jpeg'
-import F22 from './Data/Images/F22.jpeg'
 
 class App extends Component {
   render() {
@@ -13,36 +12,13 @@ class App extends Component {
         <header className="App-header">
           <h1>Fighter Aircraft Modelling</h1>
           <img className="FighterBackground" src={FighterBackground} alt="Yugioh Background" />
-          <h6>Powered by the might of somebody/somehow/mayve</h6>
+          <h6>Powered by the might of somebody/somehow/maybe</h6>
         </header>
         <section className="categories">
-          <div className="card">
-            <p className="cardTitle">1st World War</p>
-            <p className="categoriesParagraph">World War I was the first major conflict involving the large-scale use of aircraft.
-            Initially, they were used mostly for reconnaissance. Later for fighters, bombers, and trench strafers.</p>
-            <img className="cardImages" src={SE5} alt="World War 1 Aircraft" />
-          </div>
-          <div className="card">
-            <p className="cardTitle">2nd World War</p>
-            <p className="categoriesParagraph">The List of aircraft of World War II includes all the aircraft used by those countries
-            which were at war since the start to the end of the conflict.</p>
-            <img className="cardImages" src={BF109} alt="World War 1 Aircraft" />
-          </div>
-          <div className="card">
-            <p className="cardTitle">Vietnam War</p>
-            <p className="categoriesParagraph">The Vietnam War was a conflict that occurred in Vietnam, Laos, and Cambodia from 1 November
-            1955 to the fall of Saigon on 30 April 1975.</p>
-            <img className="cardImages" src={F4} alt="World War 1 Aircraft" />
-          </div>
-          <div className="card">
-            <p className="cardTitle">Fifth Generation</p>
-            <p className="categoriesParagraph">A fifth-generation jet fighter is a jet fighter classification used around the world that
-            encompasses the most advanced jet fighter generation.</p>
-            <img className="cardImages" src={F22} alt="World War 1 Aircraft" />
-          </div>
+        <Categories/>
         </section>
         <footer>
-          <h6>Powered by the might of somebody/somehow/mayve</h6>
+          <h6>Powered by the might of somebody/somehow/maybe</h6>
         </footer>
       </div >
     )
