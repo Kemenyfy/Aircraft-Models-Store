74d86552d6a5f4696fe6801040bf49eb2c6c0ba5
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-26 19:36:06 -0400

    Added Featured Products

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -9,19 +9,37 @@ class Categories extends Component {
 
     }
 
+    // newIndex = () => {
+    //     let featured = ['0', '1', ['2'], ['3']]
+    //     let index = featured[Math.floor(Math.random() * featured.length)];
+    //     return index
+    // }
+
     render() {
         return (
             <div>
-                <h4>Pick your category to see all our models.</h4>
-                <div className="categoryImages">
-                    {Object.keys(Airplanes).map((airplanesKey, i) => {
-                        return <div className="card" key={i}><Link to={`/${airplanesKey}`}>{Airplanes[airplanesKey].title}
-                            <p className="cardTitle"></p>
-                            <img className="cardImages" src={Airplanes[airplanesKey].photos[0].imageUrl} alt={Airplanes[airplanesKey].photos[0].title} />
-                        </Link></div>
-                    })}
-                </div>
+            <h3>Featured Models</h3>
+            <section className="featuredProducts">
+                {Object.keys(Airplanes).map((airplanesKey, i) => {
+                    return <div>
+                        <p className="featuredTitle">{Airplanes[airplanesKey].photos[1].title}</p>
+                        {/* <Link to={Airplanes[this.props.match.params.category]} key={i}> */}
+                        <img className="featuredImages" key={i} src={Airplanes[airplanesKey].photos[1].imageUrl} alt={Airplanes[airplanesKey].photos[1].title}/>
+                        {/* </Link> */}
+                        <p className="featuredPrice">{Airplanes[airplanesKey].photos[1].price}</p>
+                    </div>
+                })}
+            </section>
+            <h4>Pick your category to see all our models.</h4>
+            <div className="categoryImages">
+                {Object.keys(Airplanes).map((airplanesKey, i) => {
+                    return <div className="card" key={i}><Link to={`/${airplanesKey}`}>{Airplanes[airplanesKey].title}
+                        <p className="cardTitle"></p>
+                        <img className="cardImages" src={Airplanes[airplanesKey].photos[0].imageUrl} alt={Airplanes[airplanesKey].photos[0].title} />
+                    </Link></div>
+                })}
             </div>
+        </div>
         );
     }
 }
